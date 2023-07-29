b5b2dcc00e4cc1584a9d6638b545c3b490f77a2f
KVS <kevinvs003@gmail.com>
2023-07-29 17:56:33 -0500

    arreglando rutas

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -26,8 +26,8 @@ export default async function handler(
     },
     line_items: modifiedItems,
     mode: "payment",
-    success_url: `${process.env.NEXTAUTH_URL}/success`,
-    cancel_url: `${process.env.NEXTAUTH_URL}/checkout`,
+    success_url: "https://amazon-plus.vercel.app/success",
+    cancel_url: "https://amazon-plus.vercel.app/checkout",
     metadata: {
       email,
       images: JSON.stringify(items.map((item: any) => item.image)),
